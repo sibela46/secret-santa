7ecaaf62bb6d8d3c9f5817e494aebd5d75a1f3a9
sibela46 <sibelachinareva@gmail.com>
2018-12-01 11:12:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './green-present.svg';
 import './App.css';
+import soundfile from './white_christmas.mp3'
+import Sound from 'react-sound'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showName: false
+    };
+  }
+  giveName() {
+    this.setState({
+      showName: true
+    });
+    document.getElementsByClassName('jump')[0].style.display = "none";
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="jump" onClick={this.giveName.bind(this)} alt="logo" />
+          {this.state.showName ? <h1>Name</h1> : ""}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -20,6 +35,14 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Sound
+          url={soundfile}
+          playStatus={Sound.status.PLAYING}
+          playFromPosition={300 /* in milliseconds */}
+          onLoading={this.handleSongLoading}
+          onPlaying={this.handleSongPlaying}
+          onFinishedPlaying={this.handleSongFinishedPlaying}
+        />
       </div>
     );
   }
