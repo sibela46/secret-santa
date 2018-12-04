c17452b9aee6d5cffa7c5240b140471818e7cc75
sibela46 <sibelachinareva@gmail.com>
2018-12-04 14:25:57 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/secret-santa/precache-manifest.8180d763d7c1b6f43fe3ab5b7dfa539d.js"
+  "/secret-santa/precache-manifest.5890c40436379348409e718b358339e6.js"
 );
 
 workbox.clientsClaim();
