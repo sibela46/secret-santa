7ff23d8ebe97083ec63e9ea675f0ccea5e0fbb6f
sibela46 <sibelachinareva@gmail.com>
2018-12-04 13:59:53 +0000

    Updates

---
+++ precache-manifest.6a5810996b206845997f24da50c37bc2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc877a194a40abf8e9857afbc0c3802d",
+    "url": "/secret-santa/static/media/white_christmas.cc877a19.mp3"
+  },
+  {
+    "revision": "5b5dda5e9168f049844dbb763357f928",
+    "url": "/secret-santa/static/media/green-present.5b5dda5e.svg"
+  },
+  {
+    "revision": "006f19a10fe9565ff241eb53e574e8b7",
+    "url": "/secret-santa/static/media/Waving at Christmas.006f19a1.ttf"
+  },
+  {
+    "revision": "de09e440bc2a078c7d77d5f8359c7daa",
+    "url": "/secret-santa/static/media/The-Perfect-Christmas.de09e440.ttf"
+  },
+  {
+    "revision": "8532a1fc5a4d10e00b03",
+    "url": "/secret-santa/static/js/runtime~main.8532a1fc.js"
+  },
+  {
+    "revision": "067ce554bf7d63ee3d57",
+    "url": "/secret-santa/static/js/main.067ce554.chunk.js"
+  },
+  {
+    "revision": "c5baa522e03f6f17faa3",
+    "url": "/secret-santa/static/js/1.c5baa522.chunk.js"
+  },
+  {
+    "revision": "067ce554bf7d63ee3d57",
+    "url": "/secret-santa/static/css/main.15a3973e.chunk.css"
+  },
+  {
+    "revision": "726f0ea7fdd24be70e441c20ad8798c3",
+    "url": "/secret-santa/index.html"
+  }
+];
