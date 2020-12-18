0c445bde128793e850932b7b10274e5a4c3e2e69
sibela46 <sibelachinareva@gmail.com>
2020-12-18 12:50:33 +0200

    Fixing some bugs, I think it's ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss';
 import soundfile from './white_christmas.mp3'
 import Sound from 'react-sound'
 import Cookie from 'js-cookie';
-import { auth, db } from "./firebase.js";
+import { db } from "./firebase.js";
 import randomstring from 'randomstring';
 
 class App extends Component {
@@ -12,111 +12,125 @@ class App extends Component {
         super(props);
         this.state = {
             showName: false,
-            names: ["Sibela", "Nqkoi"],
+            allNames: ["Bozhidar", "Vanessa", "Teodora", "Mihail", "Vasil", "Vanq", "Ralitsa", "Iliyan", "Krasimir", "Roksena", "Pavel", "Iliyana", "Sibela", "Valya", "Asen Petrov", "Asen Simeonov", "Martina"],
+            leftNames: [],
+            drawn: [],
             pickedNumber: -1,
-            allNumers: []
+            allNumers: [],
+            currentUser: "Bozhidar",
+            soundStatus: Sound.status.STOPPED
         };
     }
     componentDidMount() {
-        db.ref("names").set(this.state.names);
+        db.ref("names").set(this.state.allNames);
         db.ref("names").on("value", snapshot => {
             let allNames = [];
             snapshot.forEach(snap => {
                 allNames.push(snap.val());
             });
-            this.setState({ names: allNames });
+            this.setState({ leftNames: allNames });
         });
-    }
-    contains(needle) {
-        // Per spec, the way to identify NaN is that it is not equal to itself
-        var findNaN = needle !== needle;
-        var indexOf;
-
-        if(!findNaN && typeof Array.prototype.indexOf === 'function') {
-            indexOf = Array.prototype.indexOf;
-        } else {
-            indexOf = function(needle) {
-                var i = -1, index = -1;
-
-                for(i = 0; i < this.length; i++) {
-                    var item = this[i];
-
-                    if((findNaN && item !== item) || item === needle) {
-                        index = i;
-                        break;
-                    }
-                }
-
-                return index;
-            };
-        }
-        return indexOf.call(this, needle) > -1;
-    }
-    showBox() {
-        document.getElementsByClassName('rules')[0].style.display = "none";
-        document.getElementsByClassName('jump')[0].style.display = "block";
-    }
-    giveName() {
-        this.setState({
-            showName: true
-        })
-        document.getElementsByClassName('jump')[0].style.display = "none";
-
-        const userCookie = Cookie.get('user');
-        if (!userCookie) {
-            const userString = randomstring.generate();
-            Cookie.set('user', userString);
-        }
-
-        let pickedName = '';
-        if (this.state.names.length == 0) {
-            pickedName = "No names left";
-        }
-
         db.ref("haveDrawn").on("value", snapshot => {
             let allUsers = [];
             snapshot.forEach(snap => {
                 allUsers.push(snap.val());
             });
-            if (allUsers.indexOf(userCookie) < 0) {
-                allUsers.push(userCookie);
-                var pickedNumber = Math.floor(Math.random() * this.state.names.length);
-                pickedName = this.state.names[pickedNumber];
-                const newNames = this.state.names.filter(name => name != pickedName);
-                this.setState({ names: newNames });
-                db.ref("names").set(newNames);
-                db.ref("haveDrawn").set(allUsers);
-            } else {
-                pickedName = "You have already drawn";
-            }
-        })
-        this.setState({
-            pickedName: pickedName
-        })
+            this.setState({ drawn: allUsers });
+        });
+    }
+    changeCurrentUser() {
+        var e = document.getElementById("names");
+        console.log(e.value);
+        this.setState({ currentUser: e.value });
+    }
+    selectName(e) {
+        document.getElementsByClassName('rules')[0].style.display = "none";
+        document.getElementsByClassName('input')[0].style.display = "flex";
+    }
+    showBox() {
+        document.getElementsByClassName('input')[0].style.display = "none";
+        document.getElementsByClassName('jump')[0].style.display = "flex";
+        document.getElementsByClassName('App-header')[0].style.display = "flex"; 
+    }
+    giveName() {
+        this.setState({ showName: true, soundStatus: Sound.status.PLAYING });
+        document.getElementsByClassName('jump')[0].style.display = "none";
+
+        let userCookie = Cookie.get('user');
+        if (!userCookie) {
+            userCookie = randomstring.generate();
+            Cookie.set('user', userCookie);
+        }
+
+        let pickedName = '';
+        const userHasDrawn = this.state.drawn.find(drawnData => drawnData.cookie === userCookie || drawnData.name === this.state.currentUser);
+        if (userHasDrawn) {
+            pickedName = userHasDrawn.pickedName;
+            this.setState({ pickedName: "Veche si teglil. Tvoqt chovek e " + pickedName });
+        } else {
+            pickedName = this.pickName();
+        }
+    }
+    pickName() {
+        let userCookie = Cookie.get('user');
+        let newlyDrawn = this.state.drawn;
+        var pickedNumber = Math.floor(Math.random() * this.state.leftNames.length);
+        let pickedName = this.state.leftNames[pickedNumber];
+        // User cannot draw this name if they have drawn themselves or if they have drawn a person who is their own secret santa
+        let userSecretSanta = this.state.drawn.find(drawnData => drawnData.name === pickedName);
+        let twoSidedSanta = false;
+        if (userSecretSanta) {
+            twoSidedSanta = userSecretSanta.pickedName === this.state.currentUser;
+        }
+        let cannotDrawThisName = pickedName === this.state.currentUser || twoSidedSanta;
+        while (cannotDrawThisName) {
+            pickedNumber = Math.floor(Math.random() * this.state.leftNames.length);
+            pickedName = this.state.leftNames[pickedNumber];
+
+            twoSidedSanta = userSecretSanta.pickedName === this.state.currentUser;
+            cannotDrawThisName = pickedName === this.state.currentUser || twoSidedSanta;
+        }
+        let obj = { cookie: userCookie, name: this.state.currentUser, pickedName: pickedName };
+        newlyDrawn.push(obj);
+        const newNames = this.state.leftNames.filter(name => name !== pickedName);
+        db.ref("names").set(newNames);
+        db.ref("haveDrawn").set(newlyDrawn);
+        this.setState({ leftNames: newNames, pickedName: pickedName });
+
+        return pickedName;
     }
     render() {
         return (
             <div className="App">
+                <Sound
+                    url={soundfile}
+                    playStatus={this.state.soundStatus}
+                    playFromPosition={400 /* in milliseconds */}
+                    onLoading={this.handleSongLoading}
+                    onPlaying={this.handleSongPlaying}
+                    onFinishedPlaying={this.handleSongFinishedPlaying}
+                />
             <div className="rules">
             <h1>Taen Dqdo Koleda Pravila:</h1>
-                <h2>Magicheskta kutiq shte reshi na kogo shte budesh ti Tainiqt Dqdo Koleda. Sled kato vidish imeto na
+                <h2>Magicheskta kutiq shte reshi na kogo da budesh Tainiqt Dqdo Koleda. Sled kato vidish imeto na
                 ekrana, si go zapishi. Shte podarish podaruk na tozi chovek (na stoinost ne po-golqma ot <span>10</span> leva) i na 
                 Nova Godina shte si razmenim podarucite.
                 </h2>
-                <button onClick={this.showBox.bind(this)}>Natisni me</button>
+                <button onClick={this.selectName.bind(this)}>Natisni me</button>
+            </div>
+            <div className="input">
+                <label className="label" for="names">Koi si ti?</label>
+                <select name="names" id="names" onChange={this.changeCurrentUser.bind(this)}>
+                    {this.state.allNames.map(name => (
+                        <option value={name}>{name}</option>
+                    ))}
+                </select>
+                <button className="small" onClick={this.showBox.bind(this)}>Iztegli ime</button>
             </div>
             <header className="App-header">
                 <img src={logo} className="jump" onClick={this.giveName.bind(this)} alt="logo" />
                 {this.state.showName ? <h1>{this.state.pickedName}</h1> : ""}
             </header>
-            {/* <Sound
-                url={soundfile}
-                playStatus={Sound.status.PLAYING}
-                playFromPosition={300 /* in milliseconds */}
-                onLoading={this.handleSongLoading}
-                onPlaying={this.handleSongPlaying}
-                onFinishedPlaying={this.handleSongFinishedPlaying}
-            /> */}
             </div>
         );
     }
